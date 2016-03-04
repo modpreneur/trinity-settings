d9de8db8a90c35fb794791963920cb15ea569640
Martin Kolek <prodejledu@gmail.com>
2016-03-04 17:19:09 +0100

    remove entrypoint.php

--- entrypoint.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-
-echo shell_exec('phpunit');
-
-while(true){
-}
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash sh
 
+chmod -R 777 Tests/Functional/var/*
+
 php Tests/Functional/bin/console.php doctrine:database:create
 php Tests/Functional/bin/console.php doctrine:schema:update --force
 
-exec php entrypoint.php
-
-
+phpunit
