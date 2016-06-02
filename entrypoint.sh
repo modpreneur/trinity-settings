4808ed3ecab69f23c7e1d82f8c7c655318286bdf
Ivo <fra01@modpreneur.com>
2016-06-02 11:58:42 +0200

    edit Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash sh
 
-chmod -R 777 Tests/Functional/var/*
-
-php Tests/Functional/bin/console.php doctrine:database:create
-php Tests/Functional/bin/console.php doctrine:schema:update --force
+composer update
 
 phpunit
