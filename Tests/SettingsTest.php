76f830357bedd64c352ea690d078709ae5fa4415
Jakub Dolníček <dolnicekjakub@gmail.com>
2017-04-07 10:56:28 +0200

    testing twig extension

--- Tests/SettingsTest.php
+++ Tests/SettingsTest.php
@@ -6,9 +6,11 @@ use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\Common\Cache\ApcuCache;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityRepository;
+use Mockery\Mock;
 use PHPUnit\Framework\TestCase;
 use Trinity\Bundle\SettingsBundle\Entity\Setting;
 use Trinity\Bundle\SettingsBundle\Manager\SettingsManager;
+use Trinity\Bundle\SettingsBundle\Twig\SettingsExtension;
 
 /**
  * Class SettingsTest
@@ -764,7 +766,7 @@ class SettingsTest extends TestCase
         $settingsEntity->setOwner($this->getUserId());
         $settingsEntity->setGroup('testingGroup');
 
-        $ex = new \Exception();
+
         $settingsRepository = $this
             ->getMockBuilder(EntityRepository::class)
             ->disableOriginalConstructor()
@@ -838,5 +840,76 @@ class SettingsTest extends TestCase
         $settings->clear($this->getUserId(), 'testingGroup');
 
         $this->assertEquals('67cm', $settings->get('height'));
+    }
+
+    public function testSettingsExtension()
+    {
+        // Mocking
+        $settingsEntity = new Setting();
+        $settingsEntity->setName('parameter');
+        $settingsEntity->setValue('value');
+
+        $settingsRepository = $this
+            ->getMockBuilder(EntityRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $settingsRepository
+            ->method('findBy')
+            ->will($this->returnValue($settingsEntity));
+
+        $settingsRepository
+            ->method('findAll')
+            ->willReturn($this->returnValue($settingsEntity));
+
+        $settings = new SettingsManager(
+            $this->getRegistry($settingsRepository),
+            [
+                'parameter' => 'value'
+            ],
+            new ApcuCache()
+        );
+
+        $extension = new SettingsExtension($settings);
+
+        $this->assertEquals('settings_extension', $extension->getName());
+        $this->assertContainsOnlyInstancesOf(\Twig_SimpleFunction::class, $extension->getFunctions());
+        $this->assertEquals('value', $extension->getSetting('parameter'));
+        $this->assertTrue($extension->hasSetting('parameter'));
+        $this->assertTrue($extension->hasSettingValue('parameter', 'value'));
+    }
+
+
+    public function testSettingsExtensionGetExceptionCatchedAndSetToNull()
+    {
+        // Mocking
+        $settingsEntity = new Setting();
+        $settingsEntity->setName('parameter');
+        $settingsEntity->setValue('value');
+
+        $settingsRepository = $this
+            ->getMockBuilder(EntityRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $settingsRepository
+            ->method('findBy')
+            ->will($this->returnValue($settingsEntity));
+
+        $settingsRepository
+            ->method('findAll')
+            ->willReturn($this->returnValue($settingsEntity));
+
+        $settings = new SettingsManager(
+            $this->getRegistry($settingsRepository),
+            [
+                'parameter' => 'value'
+            ],
+            new ApcuCache()
+        );
+
+        $extension = new SettingsExtension($settings);
+
+        $this->assertNull($extension->getSetting('foo'));
     }
 }
