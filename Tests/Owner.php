41919254b7c9088c92321ab24910b1f1405d0f2c
Jakub Dolníček <dolnicekjakub@gmail.com>
2017-04-07 09:51:26 +0200

    add Mock class for test set owner by object, and cover test for remaining in Setting entity

---
+++ Tests/Owner.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+class Owner
+{
+    protected $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
--- Tests/SettingsTest.php
+++ Tests/SettingsTest.php
@@ -53,7 +53,21 @@ class SettingsTest extends TestCase
 
     public function testEntity()
     {
+        $owner = $this->getMockBuilder(Owner::class)
+            ->setMethods(['getId'])
+            ->getMock();
+
+        // Set up the expectation for the update() method
+        // to be called only once and with the string 'something'
+        // as its parameter.
+        $owner->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue(1));
+
+
         $settingsEntity = new Setting();
+
+        $this->assertNull($settingsEntity->getId());
 
         $settingsEntity->setName('name');
         $this->assertEquals('name', $settingsEntity->getName());
@@ -67,7 +81,10 @@ class SettingsTest extends TestCase
         $settingsEntity->setOwnerId(1);
         $this->assertEquals(1, $settingsEntity->getOwnerId());
 
-        $settingsEntity->setOwner(1);
+        $settingsEntity->setOwner(2);
+        $this->assertEquals(2, $settingsEntity->getOwnerId());
+
+        $settingsEntity->setOwner($owner);
         $this->assertEquals(1, $settingsEntity->getOwnerId());
 
         $settingsEntity->setIsPrivate(false);
